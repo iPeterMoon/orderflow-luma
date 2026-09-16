3f34a9e070b49c6dc0723b5f7c15611e1185840d
normabeltraan <norma07beltran@hotmail.com>
2026-09-16 16:17:54 -0700

    fix: remove generic wildcard type and refactor lambda in tests

--- orders-api/src/test/java/mx/edu/orderflow/orders/OrderServiceTest.java
+++ orders-api/src/test/java/mx/edu/orderflow/orders/OrderServiceTest.java
@@ -19,6 +19,7 @@ class OrderServiceTest {
     @Test
     void rejectsNegativeTotal() {
         var s = new OrderService();
-        assertThrows(IllegalArgumentException.class, () -> s.create("student-1", new BigDecimal("-1")));
+        var negativeTotal = new BigDecimal("-1");
+        assertThrows(IllegalArgumentException.class, () -> s.create("student-1", negativeTotal));
     }
 }
