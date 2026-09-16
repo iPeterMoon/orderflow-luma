50c384059832a05e9ccddaabd31eaea0d1fc46ea
DeniseGarc <aliciadenise0623@gmail.com>
2026-09-16 16:03:36 -0700

    fix: add additional NotificationService tests

--- notifications-lambda/src/test/java/mx/edu/orderflow/notifications/NotificationServiceTest.java
+++ notifications-lambda/src/test/java/mx/edu/orderflow/notifications/NotificationServiceTest.java
@@ -2,9 +2,22 @@ package mx.edu.orderflow.notifications;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NotificationServiceTest {
+    @Test 
+    void rejectsNullOrderId() {
+        var s = new NotificationService();
+        assertThrows(IllegalArgumentException.class, () -> s.confirmation(null, "c-1"));
+    }
+
+    @Test 
+    void rejectsBlankOrderId() {
+        var s = new NotificationService();
+        assertThrows(IllegalArgumentException.class, () -> s.confirmation("", "c-1"));
+    }
+
     @Test
     void createsConfirmation() {
         var m = new NotificationService().confirmation("1001", "c-1");
